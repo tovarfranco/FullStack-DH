97b892a5f92d1cdca561769b8ce2d07bd30c6de4
Franco Tovar <franco.tovar.ext@kavak.com>
2021-07-21 10:39:18 -0300

    New functions

---
+++ PlayGround/12.conditionals.js
@@ -0,0 +1,20 @@
+//IF ternario: se usa para abreviar codigo de if.
+// condicion ? se_cumple:no_se_cumple
+let a = 5
+let b = 6
+
+a > b ? console.log("a es mas grande que b"):console.log("a es menor que b");
+
+//Switch: permite evaluar varias posibilidades para un valor.
+
+switch (expresion){
+    case valorA:
+        //codigo
+        break;
+    case valorB:
+        //codigo
+        break;
+    default:
+        //codigo
+        break;
+}
